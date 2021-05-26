187e14ef5e787efb80ba252551524201aa728baf
mactanex <malthedj@hotmail.com>
2021-05-26 12:58:56 +0200

    comments for node a static

--- NodeAStatic/nodeAStatic.c
+++ NodeAStatic/nodeAStatic.c
@@ -16,17 +16,17 @@
 
 int ACK_n = 0;
 
-//static linkaddr_t addr_nodeC =     {{0x43, 0xf5, 0x6e, 0x14, 0x00, 0x74, 0x12, 0x00}};
-static linkaddr_t addr_nodeC =     {{0x77, 0xb7, 0x7b, 0x11, 0x0, 0x74, 0x12, 0x00 }};
- //static linkaddr_t addr_nodeC = {{0x03, 0x03, 0x03, 0x00, 0x03, 0x74, 0x12, 0x00}}; // coojas
+//static linkaddr_t addr_nodeC =     {{0x43, 0xf5, 0x6e, 0x14, 0x00, 0x74, 0x12, 0x00}}; // physical domain
+static linkaddr_t addr_nodeC =     {{0x77, 0xb7, 0x7b, 0x11, 0x0, 0x74, 0x12, 0x00 }}; // physical domain
+ //static linkaddr_t addr_nodeC = {{0x03, 0x03, 0x03, 0x00, 0x03, 0x74, 0x12, 0x00}}; // cooja
 
-static unsigned long to_10milseconds(uint64_t time)
+static unsigned long to_milliseconds(uint64_t time)  // helper function to calcule energest values to milliseconds
 {
   return (unsigned long)(time / 62.5 /*ENERGEST_SECOND*/);
 }
 
 
-uint8_t checksum(uint8_t* buffer, uint8_t len)
+uint8_t checksum(uint8_t* buffer, uint8_t len) // function to calculate a naive checksum
 {
       size_t i;
       uint8_t checksum = 0;
@@ -37,42 +37,31 @@ uint8_t checksum(uint8_t* buffer, uint8_t len)
       return checksum;
 }
 
-void sendPayload(){
+void sendPayload(){ // function to send and construct a package.
     uint8_t payloadData[64] = {1,2,3,4,5,6,7};
     JumpPackage payload;
-    payload.sender = (linkaddr_t){{0x77, 0xb7, 0x7b, 0x11, 0x00, 0x74, 0x12, 0x00}};
-    payload.destination = (linkaddr_t){{0x43, 0xf5, 0x6e, 0x14, 0x00, 0x74, 0x12, 0x00}};
+    payload.sender = (linkaddr_t){{0x77, 0xb7, 0x7b, 0x11, 0x00, 0x74, 0x12, 0x00}}; //source adddress
+    payload.destination = (linkaddr_t){{0x43, 0xf5, 0x6e, 0x14, 0x00, 0x74, 0x12, 0x00}}; // sink addrses
     memcpy(payload.payload,payloadData,64);
     payload.sequenceNumber = 1;
     payload.checksum = checksum(payloadData,7);
     payload.length = 7;
+    // assign to nullnet buffer
     nullnet_buf = (uint8_t *)&payload;
-
     nullnet_len = sizeof(payload);
-  
-
-    NETSTACK_NETWORK.output(&addr_nodeC);
-    //energest_flush();
-    size_t i;
-    for ( i = 0; i < 64; i++)
-    {
-      if(payload.payload[i]==0) {
-      break;
-    }
-    //LOG_INFO("Sent message %u \n ", payload.payload[i] );
-  }
+    NETSTACK_NETWORK.output(&addr_nodeC); //transmit the package
 }
 
 
-void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest){
+void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest){ // callback function 
     uint8_t ack;
     memcpy(&ack, data, sizeof(ack));
-    if (ack == 1) {
+    if (ack == 1) { // if acknowledge received
      /*  LOG_INFO("Acknowledged received from: ");
       LOG_INFO_LLADDR(src);
       LOG_INFO_("\n"); */
       ACK_n++;
-    } else if (ack == 255) {
+    } else if (ack == 255) { // if negative acknowledge received
       /* LOG_INFO("Not acknowledged received from: ");
       LOG_INFO_LLADDR(src);
       LOG_INFO_("\n"); */
@@ -86,9 +75,9 @@ PROCESS_THREAD(nodeA, ev, data)
 {
     PROCESS_BEGIN();
     static struct etimer periodic_timer;
-    static int index = 0;
+    static int index = 0; // index to evaluate when to stop transmitting packages.
     printf("STARTING NODE A,,, \n"); 
-    nullnet_set_input_callback(input_callback);
+    nullnet_set_input_callback(input_callback); // set input callback
     etimer_set(&periodic_timer, 250);
     SENSORS_ACTIVATE(button_sensor);
     
@@ -100,7 +89,7 @@ PROCESS_THREAD(nodeA, ev, data)
 
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
         
-        etimer_reset(&periodic_timer);
+        etimer_reset(&periodic_timer); // reset timer
 
 
 
@@ -109,20 +98,20 @@ PROCESS_THREAD(nodeA, ev, data)
 
     printf("Number of ACKs received: %i", ACK_n);
           /* Update all energest times. */
-        energest_flush();
-
-        printf("\nEnergest:\n");
-        printf("10ms *: CPU          %4lums LPM      %4lus DEEP LPM %4lums  Total time %lums\n",
-           to_10milseconds(energest_type_time(ENERGEST_TYPE_CPU)),
-           to_10milseconds(energest_type_time(ENERGEST_TYPE_LPM)),
-           to_10milseconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
-           to_10milseconds(ENERGEST_GET_TOTAL_TIME()));
-        printf("10ms *: Radio LISTEN %4lums TRANSMIT %4lums OFF      %4lums\n",
-           to_10milseconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
-           to_10milseconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
-           to_10milseconds(ENERGEST_GET_TOTAL_TIME()
-                      - energest_type_time(ENERGEST_TYPE_TRANSMIT)
-                      - energest_type_time(ENERGEST_TYPE_LISTEN)));
+    energest_flush();
+    //printout energest measurements
+    printf("\nEnergest:\n");
+    printf("10ms *: CPU          %4lums LPM      %4lus DEEP LPM %4lums  Total time %lums\n",
+        to_milliseconds(energest_type_time(ENERGEST_TYPE_CPU)),
+        to_milliseconds(energest_type_time(ENERGEST_TYPE_LPM)),
+        to_milliseconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
+        to_milliseconds(ENERGEST_GET_TOTAL_TIME()));
+    printf("10ms *: Radio LISTEN %4lums TRANSMIT %4lums OFF      %4lums\n",
+        to_milliseconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
+        to_milliseconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
+        to_milliseconds(ENERGEST_GET_TOTAL_TIME()
+                  - energest_type_time(ENERGEST_TYPE_TRANSMIT)
+                  - energest_type_time(ENERGEST_TYPE_LISTEN)));
         
     PROCESS_END();
 }
